87aa3a2c1d0b3d20a17ec1efb77a22f9cef58f05
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-07-24 14:18:41 -0400

    Reversion

--- editor/js/ReactMenu.js
+++ editor/js/ReactMenu.js
@@ -1,38 +1,9 @@
-// import { Menu } from "@szhsin/react-menu"
-
-// import React from "react";
-
-function Slice() {
-    console.log("Slicing");
-}
-
-class TextField extends React.Component {
-
-}
-
-class SliceButton extends React.Component {
-
-
-    render() {
-        React.createElement("button", {
-            onclick: Slice()
-        }, "Slice");
-    }
-}
-
-class HelloWorld extends React.Component {
-    render() {
-        return React.createElement("h1", null, "Hello World React");
-    }
-}
 
 class ReactMenu extends React.Component {
     render() {
-
-        return React.createElement(SliceButton, null);
-        // return React.createElement(Menu, {
-        //     menuButton: /*#__PURE__*/React.createElement(MenuButton, null, "Open menu")
-        //   }, /*#__PURE__*/React.createElement(MenuItem, null, "New File"), /*#__PURE__*/React.createElement(MenuItem, null, "Save"), /*#__PURE__*/React.createElement(MenuItem, null, "Close Window"));
+        return React.createElement(Menu, {
+            menuButton: /*#__PURE__*/React.createElement(MenuButton, null, "Open menu")
+          }, /*#__PURE__*/React.createElement(MenuItem, null, "New File"), /*#__PURE__*/React.createElement(MenuItem, null, "Save"), /*#__PURE__*/React.createElement(MenuItem, null, "Close Window"));
     }
 }
 
