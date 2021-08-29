95cfc92b40e261e70f108ada9c7182f7d8e004ac
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-08-29 12:40:17 -0400

    Revert "Nav selection capability added"
    This reverts commit d3fd6805e620f6f0fe327d9d374ec4dfb7a324e0.

--- zengerwriter/src/NavElement.js
+++ zengerwriter/src/NavElement.js
@@ -1,36 +1,21 @@
-import { useRef, useState, forwardRef, useImperativeHandle } from "react"
+import { useRef } from "react"
 
-function NavElement(props, ref) {
-
-    var [myClass, setClass] = useState(props.className);
-
-    // for ref forwarding
-    // const inputRef = useRef();
-    // useImperativeHandle(ref, () => ({
-    //     focus: () => {
-    //         inputRef.current.focus();
-    //     }
-    // }));
-
+function NavElement(props) {
 
     var name = useRef(props.name);
     function handleClick() {
         props.handleClick();
-        // setClass("selected-nav-element");
     }
 
     return (
+        <div onClick={handleClick} >
 
-        <div onClick={handleClick} ref={ref} className={myClass}>
-            <div className="noselect">
-                <p className="child-text">
-                    {name.current}
-                </p>
-            </div>
+            <div class="noselect">
+                {name.current}
+            </div>            
+
         </div>
-        
     )
 }
-NavElement = forwardRef(NavElement);
 
 export { NavElement }
