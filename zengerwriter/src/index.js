2c9dbf8d321c50783a8cf5b4e97791573be4c272
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-08-29 12:43:37 -0400

    Revert "Multi-page functionality"
    This reverts commit 12b3d5f8b61e4d690397ce1b960df03483c6b02a.

--- zengerwriter/src/index.js
+++ zengerwriter/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Queue from './Queue';
+import Prepare from './Prepare';
+import Slice from './Slice';
+import { Banner } from './Banner'
+import { Dividers } from './Dividers';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +16,11 @@ var username = "testman";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App username={username}/>
+    <Banner />
+    <Slice username={username} serverIP="54.159.145.255"/>
+    <Queue username={username} serverIP="54.159.145.255"/>
+    <Prepare username={username} serverIP="54.159.145.255"/>
+    <Dividers />
   </React.StrictMode>,
   document.getElementById('root')
 );
