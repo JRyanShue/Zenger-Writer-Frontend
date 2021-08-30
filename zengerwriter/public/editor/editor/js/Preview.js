cf9e5e39dc57b70dd1c0f348478ac942cb14d925
JRyanShue <80791522+JRyanShue@users.noreply.github.com>
2021-08-30 17:07:16 -0400

    Test scene in layer view

--- zengerwriter/public/editor/editor/js/Preview.js
+++ zengerwriter/public/editor/editor/js/Preview.js
@@ -29,8 +29,51 @@ function Preview( editor ) {
     // Renderer 
 
     var renderer = new THREE.WebGLRenderer(); 
-    this.preview.dom.appendChild( renderer.domElement );
+    renderer.setClearColor(0xffffff, 0);
+    renderer.setSize( window.innerWidth, window.innerHeight );
 
+    // Scene
+
+    var scene = new THREE.Scene();
+
+    // CAMERA 
+
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+    camera.position.x = 17;
+    camera.position.y = 12;
+    camera.position.z = 13;
+    camera.lookAt(scene.position);
+
+    // GEOMETRY & MATERIALS
+
+    var cubeGeometry = new THREE.BoxGeometry(3, 3, 3);
+    var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff55ff});
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    scene.add(cube);
+    cube.position.z = 4;
+
+    var ballGeometry = new THREE.SphereGeometry(3, 16, 16);
+    var ballMaterial = new THREE.MeshPhongMaterial({color: 0x33aaff});
+    var ball = new THREE.Mesh(ballGeometry, ballMaterial);
+    scene.add(ball);
+    ball.position.z = -5;
+
+    var floorGeometry = new THREE.BoxGeometry(30, 1, 30);
+    var floorMaterial = new THREE.MeshBasicMaterial({color: 0x656587});
+    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    scene.add(floor);
+    floor.position.y = -3;
+    floor.receiveShadow = true;
+
+    // LIGHT
+
+    var spot1 = new THREE.SpotLight(0xffffff);
+    spot1.position.set(10, 100, -50);
+    scene.add(spot1);
+
+    renderer.render( scene, camera );
+
+    this.preview.dom.appendChild( renderer.domElement );
     container.add( this.preview );
 
     return container;
